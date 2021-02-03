51a07aff90d9b987f751225e301de52c572f46e3
mskat <zur.katarzyna@gmail.com>
2021-02-03 12:10:57 +0000

    Declare method asking for user's email
    The created method asks the user for an email and performs the validation.

--- app.js
+++ app.js
@@ -4,3 +4,22 @@ const cheerio = require('cheerio');
 const knwl = require('knwl.js');
 const prompt = require('prompt');
 
+//email validation
+let validation = {
+    properties: {
+        email: {
+            pattern: /^[a-z\d]+[\w\d.-]*@(?:[a-z\d-]+\.){1,5}[a-z]{2,6}$/i,
+            message: 'Give a proper email address!',
+            required: true
+        }
+    }
+};
+
+prompt.start();
+
+//method getting an email address from the user
+prompt.get(validation, function (err, result) {
+    if (err) { return onErr(err); }
+    console.log('Received email: ' + result.email);
+});
+
